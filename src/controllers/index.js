57403f8add1b41e2bfaf1abb63baa162154be277
Galih Trisna <galihtrisna666@gmail.com>
2025-10-21 18:20:26 +0700

    typo capitalcase

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,9 @@
 const eventController = require("./event.controller");
 const participantController = require("./participant.controller");
-const PaymentController = require("./payment.controller");
+const paymentController = require("./payment.controller");
 
 module.exports = {
     eventController,
     participantController,
-    PaymentController
+    paymentController
 };
